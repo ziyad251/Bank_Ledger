46a7194b378e9392b789c69aa2279301f08a4b15
mahammad ziyad <mahammadziyad251@gmail.com>
2026-08-01 10:14:35 +0530

    Create Account  model

---
+++ src/models/account.model.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose")
+const ledgerModel = require("./ledger.model")
+
+const accountSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: [ true, "Account must be associated with a user" ],
+        index: true
+    },
+    status: {
+        type: String,
+        enum: {
+            values: [ "ACTIVE", "FROZEN", "CLOSED" ],
+            message: "Status can be either ACTIVE, FROZEN or CLOSED",
+        },
+        default: "ACTIVE"
+    },
+    currency: {
+        type: String,
+        required: [ true, "Currency is required for creating an account" ],
+        default: "INR"
+    }
+}, {
+    timestamps: true
+})
+
+accountSchema.index({ user: 1, status: 1 })
+
+accountSchema.methods.getBalance = async function () {
+
+    const balanceData = await ledgerModel.aggregate([
+        { $match: { account: this._id } },
+        {
+            $group: {
+                _id: null,
+                totalDebit: {
+                    $sum: {
+                        $cond: [
+                            { $eq: [ "$type", "DEBIT" ] },
+                            "$amount",
+                            0
+                        ]
+                    }
+                },
+                totalCredit: {
+                    $sum: {
+                        $cond: [
+                            { $eq: [ "$type", "CREDIT" ] },
+                            "$amount",
+                            0
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                balance: { $subtract: [ "$totalCredit", "$totalDebit" ] }
+            }
+        }
+    ])
+
+    if (balanceData.length === 0) {
+        return 0
+    }
+
+    return balanceData[ 0 ].balance
+
+}
+
+
+const accountModel = mongoose.model("account", accountSchema)
+
+
+
+module.exports = accountModel
