d20dbfc1aa544142847d70079f980b7bcda440db
mahammad ziyad <mahammadziyad251@gmail.com>
2026-08-02 11:36:47 +0530

    Add blacklist validation to authentication middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,10 +1,11 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const tokenBlackListModel = require("../models/blackList.model")
+
 
 async function authMiddleware(req, res, next) {
 
-    const token = req.cookies.token ||
-        req.headers.authorization?.split(" ")[1]
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
 
     if (!token) {
         return res.status(401).json({
@@ -12,7 +13,16 @@ async function authMiddleware(req, res, next) {
         })
     }
 
+    const isBlacklisted = await tokenBlackListModel.findOne({ token })
+
+    if (isBlacklisted) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
+        })
+    }
+
     try {
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         const user = await userModel.findById(decoded.userId)
