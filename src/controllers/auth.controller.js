d38d6fbfe9a2b19597dc78e3485fcace8d208e75
mahammad ziyad <mahammadziyad251@gmail.com>
2026-08-02 11:42:40 +0530

    Implement user login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -44,6 +44,48 @@ async function userRegisterController(req, res) {
     await emailService.sendRegistrationEmail(user.email, user.name)
 }
 
+/**
+ * - User Login Controller
+ * - POST /api/auth/login
+ */
+async function userLoginController(req, res) {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email }).select("+password")
+
+    if (!user) {
+        return res.status(401).json({
+            message: "Email or password is INVALID"
+        })
+    }
+
+    const isValidPassword = await user.comparePassword(password)
+
+    if (!isValidPassword) {
+        return res.status(401).json({
+            message: "Email or password is INVALID"
+        })
+    }
+
+    const token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "3d" }
+    )
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        user: {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
+}
+
 module.exports = {
-    userRegisterController
+    userRegisterController,
+    userLoginController
 }
