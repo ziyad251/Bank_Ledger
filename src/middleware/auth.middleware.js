4ae9bf316aa4e421e85e2139f5b8ea8b21da0015
mahammad ziyad <mahammadziyad251@gmail.com>
2026-08-02 11:37:48 +0530

    Add system user authorization middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,9 +3,10 @@ const jwt = require("jsonwebtoken")
 const tokenBlackListModel = require("../models/blackList.model")
 
 
+
 async function authMiddleware(req, res, next) {
 
-    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
 
     if (!token) {
         return res.status(401).json({
@@ -37,7 +38,47 @@ async function authMiddleware(req, res, next) {
         })
     }
 }
+async function authSystemUserMiddleware(req, res, next) {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is missing"
+        })
+    }
+
+    const isBlacklisted = await tokenBlackListModel.findOne({ token })
+
+    if (isBlacklisted) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(decoded.userId).select("+systemUser")
+        if (!user.systemUser) {
+            return res.status(403).json({
+                message: "Forbidden access, not a system user"
+            })
+        }
+
+        req.user = user
+
+        return next()
+    }
+    catch (err) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
+        })
+    }
+
+}
 
 module.exports = {
-    authMiddleware
+    authMiddleware,
+    authSystemUserMiddleware
 }
